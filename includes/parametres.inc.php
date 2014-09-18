02197925fa3edcefa585895bdc6b0142e7a743b0
btssio <btssio@btssio-VirtualBox>
2014-09-18 15:54:39 +0200

    Projet Gestage -Modif nom de base

--- includes/parametres.inc.php
+++ includes/parametres.inc.php
@@ -4,7 +4,7 @@
 define('VERSION_APPLICATION', 'juin 2014 - initiale');
 
 // Accès base de données
-define('DSN', 'mysql:host=localhost;dbname=PPE3-Gestage');
+define('DSN', 'mysql:host=localhost;dbname=ppe');
 define('USER', 'root');
 define('MDP', 'joliverie');
 
